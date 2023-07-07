e99a02f542cbf04d193e68faa1c823bf235f02c6
flawlezfox <armanarakelyan33@gmail.com>
2023-07-07 21:02:10 +0400

    styled header and made it responsive to side menu

--- src/common/el/header/index.tsx
+++ src/common/el/header/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import "./index.css";
 
 const Header = () => {
     return (
         <div className="header-container">
-
+            <Link to="/" className="header-title">Organizer</Link>
+            <hr />
         </div>
     );
 }
